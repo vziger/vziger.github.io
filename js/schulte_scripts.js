4eb4675f4566d38262d1dddd79560762b7655ccc
Vzhiger <43498742+vziger@users.noreply.github.com>
2024-04-05 13:58:09 +0200

    Update schulte_scripts.js

--- js/schulte_scripts.js
+++ js/schulte_scripts.js
@@ -1,3 +1,11 @@
+const ELEMENTS_ALPHABET_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+const TABLE_TYPE_DIGITS  = 1
+const TABLE_TYPE_LETTERS = 2
+const TABLE_TYPE_GORBOV  = 3
+
+const TABLE_HINT_SHOW = 1
+
 let timer_id
 let timer_node
 
@@ -12,6 +20,8 @@ let results_container
 
 let find_number_title_node
 let find_number_node
+
+let root_doc
 
 
 function ready() {
@@ -31,6 +41,8 @@ function ready() {
 
     find_number_title_node = document.getElementById('find_number_title')
     find_number_node = document.getElementById('find_number')
+
+    root_doc = document.documentElement;
 }
 
 
@@ -48,6 +60,7 @@ function create_new_cells_for_table(number, grid_table) {
         new_cell.setAttribute('class', 'cell')
         new_cell.setAttribute('data-number', i+1)
         new_cell.innerHTML = '*'
+        new_cell.style.setProperty('padding-top', '16px')
         grid_table.appendChild(new_cell)
     }
 }
@@ -63,6 +76,16 @@ function selected_size_switcher(size){
     grid_table.style.setProperty('grid-template-columns', 'repeat(' + size + ', 120px)')
 }
 
+
+function generate_numbers_array(size) {
+    let arr = new Array(size)
+    for (let i = 0; i < size; i++) {
+        arr[i] = i + 1
+    }
+    return arr
+}
+
+
 function kanzas_city_shuffle(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -71,16 +94,17 @@ function kanzas_city_shuffle(arr) {
     return arr
 }
 
+
 function display_hide_containers(list_elements, show_hide_flag) {
     for (let j = 0; j < list_elements.length ; j++) {
         list_elements[j].style.setProperty('display', show_hide_flag)
     }
 }
 
+
 function show_settings() {
-    const root = document.documentElement;
-    root.style.setProperty('--cell-blur', 'blur(8px)');
-    root.style.setProperty('--cell-cursor', 'default');
+    root_doc.style.setProperty('--cell-blur', 'blur(8px)');
+    root_doc.style.setProperty('--cell-cursor', 'default');
     
     display_hide_containers(all_switchers_container, 'block');
     display_hide_containers(results_container, 'none');
@@ -93,12 +117,48 @@ function show_settings() {
     btn_again.style.setProperty('display', 'none');
 }
 
+
+function blink(cell) {
+    cell.style.setProperty('background-color','#63BE63')
+        setTimeout(function() {
+            cell.style.setProperty('background-color','#E9E9E9');
+        }, 50)
+}
+
+
+function seconds_to_time(total_seconds) {
+    let hours   = Math.floor(total_seconds / 3600)
+    let minutes = Math.floor((total_seconds - (hours * 3600)) / 60)
+    let seconds = total_seconds - (hours * 3600) - (minutes * 60)
+
+    seconds = Math.round(seconds * 100) / 100
+
+    let time_to_show = ''
+    if (hours != 0) {
+        time_to_show += hours + ':'
+    }
+    time_to_show += (minutes < 10 ? '0' + minutes : minutes)
+    time_to_show += ':' + (seconds  < 10 ? '0' + seconds : seconds)
+    return time_to_show
+}
+
+
+function set_asterisks_into_cells() {
+    let grid_cells = document.getElementsByClassName('cell')
+    for (let i = grid_cells.length - 1; i >=0 ; --i) {
+        grid_cells[i].innerHTML = '*'
+        grid_cells[i].style.setProperty('padding-top', '16px')
+    }
+}
+
+
 function stop_schulte() {
     clearInterval(timer_id);
 
-    const root = document.documentElement;
-    root.style.setProperty('--cell-blur', 'blur(8px)');
-    root.style.setProperty('--cell-cursor', 'default');
+    root_doc.style.setProperty('--cell-blur', 'blur(8px)');
+    root_doc.style.setProperty('--cell-cursor', 'default');
+
+    set_asterisks_into_cells();
     
     display_hide_containers(all_switchers_container, 'block');
     display_hide_containers(results_container, 'none');
@@ -108,24 +168,12 @@ function stop_schulte() {
 
     btn_stop.style.setProperty('display', 'none');
     btn_start.style.setProperty('display', 'block');
-    
 }
 
 function start_schulte(){
-    const root = document.documentElement;
-
-    const ELEMENTS_ALPHABET_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-
     const TABLE_SIZE_SELECTED = document.querySelector('input[type="radio"][name="btnradio-size"]:checked').getAttribute('data-size')
     const TABLE_TYPE_SELECTED = document.querySelector('input[type="radio"][name="btnradio-type"]:checked').getAttribute('data-type')
     const TABLE_HINT_SELECTED = document.querySelector('input[type="radio"][name="btnradio-hint"]:checked').getAttribute('data-hint')
-
-
-    const TABLE_TYPE_DIGITS  = 1
-    const TABLE_TYPE_LETTERS = 2
-    const TABLE_TYPE_GORBOV  = 3
-
-    const TABLE_HINT_SHOW = 1
 
     const NUMBER_ELEMENTS = TABLE_SIZE_SELECTED * TABLE_SIZE_SELECTED
     let cells_data        = new Array(NUMBER_ELEMENTS)
@@ -140,7 +188,6 @@ function start_schulte(){
     
     find_number_title_node.style.setProperty('visibility', 'hidden')
 	find_number_node.style.setProperty('visibility', 'hidden')
-
 
     if (TABLE_HINT_SELECTED == TABLE_HINT_SHOW) {
         find_number_title_node.style.setProperty('visibility', 'visible')
@@ -185,86 +232,52 @@ function start_schulte(){
 
     function timer() {
         timer_seconds++;
-        let time_string = seconds_to_time(timer_seconds)
-        timer_node.innerHTML = time_string
+        timer_node.innerHTML = seconds_to_time(timer_seconds)
     }
-
-    function seconds_to_time(total_seconds) {
-        let hours   = Math.floor(total_seconds / 3600)
-        let minutes = Math.floor((total_seconds - (hours * 3600)) / 60)
-        let seconds = total_seconds - (hours * 3600) - (minutes * 60)
-
-        seconds = Math.round(seconds * 100) / 100
-
-        let time_to_show = ''
-        if (hours != 0) {
-            time_to_show += hours + ':'
-        }
-        time_to_show += (minutes < 10 ? '0' + minutes : minutes)
-        time_to_show += ':' + (seconds  < 10 ? '0' + seconds : seconds)
-        return time_to_show
-    }
-
-    function generate_numbers_array(size) {
-		let arr = new Array(size)
-		for (let i = 0; i < size; i++) {
-			arr[i] = i + 1
-		}
-		return arr
-	}
 
     function set_data_to_cells(data, table_size) {
         for (let i = 0; i < table_size; i++) {
-            let str = '.cell[data-number="' + (i+1) + '"]'
-            let cell = document.querySelector(str)
+            let cell = document.querySelector('.cell[data-number="' + (i+1) + '"]')
             cell.innerHTML = data[i]
+            cell.style.removeProperty('padding-top');
             
             cell.addEventListener('click', check_click);
         }
-        root.style.setProperty('--cell-blur', 'blur(0px)');
-        root.style.setProperty('--cell-cursor', 'pointer');
+        root_doc.style.setProperty('--cell-blur', 'blur(0px)');
+        root_doc.style.setProperty('--cell-cursor', 'pointer');
     }
 
     function check_click(event) {
         let cell = event.target || event.srcElement;
         if (cell.innerHTML == '' + straight_data[current_char_pos] && current_char_pos == NUMBER_ELEMENTS - 1) {
             clearInterval(timer_id);
+            blink(cell)
 
             btn_stop.style.setProperty('display', 'none')
             btn_again.style.setProperty('display', 'block')
             btn_show_settings.style.setProperty('visibility', 'visible')
 
-            blink(cell)
-
             find_number_title_node.style.setProperty('visibility', 'visible')
             find_number_node.style.setProperty('visibility', 'visible')
+
             find_number_title_node.innerHTML = 'Всего ошибок: '
             find_number_node.innerHTML = number_errors
+
             current_char_pos++
 
-            console.log('Верно!!! → ' + cell.innerHTML)
+            console.log('Верно → ' + cell.innerHTML)
         }
         if (cell.innerHTML == '' + straight_data[current_char_pos]) {
-            current_char_pos++
-            find_number_node.innerHTML = straight_data[current_char_pos]
+            find_number_node.innerHTML = straight_data[++current_char_pos]
             blink(cell);
 
-            console.log('Верно!!! → ' + cell.innerHTML);
+            console.log('Верно → ' + cell.innerHTML);
         }
         else {
-            console.log('number_errors = ' + number_errors);
             number_errors++
-            
-            console.log('НЕ Верно!!! → ' + cell.innerHTML);
-            console.log('number_errors = ' + number_errors);
+
+            console.log('Не верно → ' + cell.innerHTML);
+            console.log('Количество ошибок = ' + number_errors);
         }
     }
-
-    function blink(cell) {
-        cell.style.setProperty('background-color','#63BE63')
-            setTimeout(function() {
-                cell.style.setProperty('background-color','#E9E9E9');
-            }, 50)
-    }
-
 }
