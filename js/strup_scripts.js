689235bb4a1e9e8fd7b73841969ae26be98f6d68
Vzhiger <43498742+vziger@users.noreply.github.com>
2025-01-18 23:49:54 +0200

    update strup scripts

--- js/strup_scripts.js
+++ js/strup_scripts.js
@@ -5,54 +5,45 @@ const BREAK_WORD  = 'ХЛОПОК'
 const WHAT_COLORED_SETTING__BCKGR = 2
 const DELAY  = 1500
 
+
 let delays
 let frequency_break_word
 
-let box_margin = 48
-let box_size
 let strup_box
-let strup_playground
+let strup_word
+let strup_close_btn
 let colored_property
 let bw_property
-let strup_close_btn
 
 let input_velocity
-let velocity = 1
-let root_doc
 
 let exercise_interval_id
 
 let navbar_node
 let navbar_container
+let navbar_btn
 
-let btn_navbar
 let error_text_node
 
 
 function ready_strup() {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-    
-    root_doc = document.documentElement
 
-    delays = make_delays_array(300, 300, 10)
+    delays   = make_delays_array(300, 300, 10)
     console.log(delays)
-
+    
     strup_box        = document.getElementById('strup_box')
-    strup_playground = document.getElementById('strup_playground')
+    strup_word       = document.getElementById('strup_word')
     strup_close_btn  = document.getElementById('close_exercise')
 
-    // TODO: get_property_int_value()
-    box_size = getComputedStyle(root_doc, null).getPropertyValue('--box-for-strup-size')
-    box_size = +box_size.substring(0, box_size.length - 2)
+    input_velocity   = document.getElementById('input_velocity')
 
-    input_velocity = document.getElementById('input_velocity')
-    
-    navbar_node = document.getElementById('navbar')
+    navbar_node      = document.getElementById('navbar')
     navbar_container = document.getElementById('navbar_container')
+    navbar_btn       = document.getElementById('navbar_button')
 
-    btn_navbar = document.getElementById('navbar_button')
-    error_text_node = document.getElementById('switchers_error')
+    error_text_node  = document.getElementById('switchers_error')
 
     set_event_listeners_to_change_velocity_buttons()
     set_event_listener_escape_fullscreen_mode(strup_box, stop_strup)
@@ -70,7 +61,6 @@ function set_event_listeners_to_change_velocity_buttons(){
                 (velocity_value < 10 && dv > 0)
             ){
                 input_velocity.value = dv + velocity_value
-                velocity = +input_velocity.value
             }
         })
     }
@@ -78,8 +68,8 @@ function set_event_listeners_to_change_velocity_buttons(){
 
 
 function show_hide_lorem_colors(show) {
-    let property_value = show == true ? 'block' : 'none'
-    lorem_colors = document.getElementsByName("text-color")
+    const property_value = show == true ? 'block' : 'none'
+    let lorem_colors = document.getElementsByName("text-color")
     for (let i = lorem_colors.length - 1; i >= 0 ; --i) {
         lorem_colors[i].style.setProperty('display', property_value)
     }
@@ -88,29 +78,29 @@ function show_hide_lorem_colors(show) {
 
 function resize_playground(make_bigger) {
     if(make_bigger) {
-        strup_playground.style.setProperty('display', 'block')
-        strup_playground.style.setProperty('visibility', 'hidden')
+        strup_word.style.setProperty('display', 'block')
+        strup_word.style.setProperty('visibility', 'hidden')
         strup_close_btn.style.setProperty('display', 'block')
         
         let ww = document.documentElement.clientWidth
         let hh = document.documentElement.clientHeight
-        let menu_btn_width = Number(btn_navbar.offsetWidth)
+        let menu_btn_width = Number(navbar_btn.offsetWidth)
 
         let pl = get_property_int_value(navbar_container, 'padding-left')
         let pt = get_property_int_value(navbar_node, 'padding-top')
 
-        let left = menu_btn_width + 2*pl
-        let top  = pt
-        let width = ww - menu_btn_width - 3*pl
+        let left   = menu_btn_width + 2*pl
+        let top    = pt
+        let width  = ww - menu_btn_width - 3*pl
         let height = hh - 2*pt
 
         set_pos_dom_element(strup_box, 'absolute', left, top)
         strup_box.style.setProperty('width', width + 'px')
-        strup_box.style.setProperty('height', height + 'px')   
+        strup_box.style.setProperty('height', height + 'px')
     }
     else {
-        strup_playground.style.setProperty('display', 'none')
-        strup_playground.style.setProperty('visibility', 'visible')
+        strup_word.style.setProperty('visibility', 'visible')
+        strup_word.style.setProperty('display', 'none')
         strup_close_btn.style.setProperty('display', 'none')
 
         strup_box.style.removeProperty('left')
@@ -129,24 +119,28 @@ function stop_strup(){
 }
 
 
-function get_word() {
-    strup_playground.style.visibility = 'hidden'
-    strup_playground.innerHTML = get_random_int(0, 100) > frequency_break_word ? COLOR_NAMES[get_random_int(0, COLOR_NAMES.length - 1)] : BREAK_WORD
-    
-    let left = get_random_int(0, strup_box.offsetWidth - strup_playground.offsetWidth)
-    let top  = get_random_int(0, strup_box.offsetHeight - strup_playground.offsetHeight)
-
-    set_pos_dom_element(strup_playground, '', left, top)
-    
-    let color = COLOR_HEX[get_random_int(0, COLOR_HEX.length - 1 )]
-    strup_playground.style.setProperty(colored_property, color)
+function make_yellow_contrast(color) {
     if (color == '#ffcd4d') {
-        strup_playground.style.setProperty('text-shadow', '.5px .5px #C58D00')
+        strup_word.style.setProperty('text-shadow', '.5px .5px #C58D00')
     }
     else {
-        strup_playground.style.removeProperty('text-shadow')
+        strup_word.style.removeProperty('text-shadow')
     }
-    strup_playground.style.visibility = 'visible';
+}
+
+function get_word() {
+    strup_word.style.visibility = 'hidden'
+    strup_word.innerHTML = (get_random_int(0, 100) > frequency_break_word) ? COLOR_NAMES[get_random_int(0, COLOR_NAMES.length - 1)] : BREAK_WORD
+    
+    let left = get_random_int(0, strup_box.offsetWidth - strup_word.offsetWidth)
+    let top  = get_random_int(0, strup_box.offsetHeight - strup_word.offsetHeight)
+
+    set_pos_dom_element(strup_word, '', left, top)
+    
+    let color = COLOR_HEX[get_random_int(0, COLOR_HEX.length - 1 )]
+    strup_word.style.setProperty(colored_property, color)
+    make_yellow_contrast(color)
+    strup_word.style.visibility = 'visible';
 }
 
 
@@ -166,7 +160,7 @@ function start_strup() {
         error_text_node.style.setProperty('visibility', 'hidden')
         show_hide_lorem_colors(false)
         resize_playground(true)
-        strup_playground.style.setProperty(bw_property, 'white')
+        strup_word.style.setProperty(bw_property, 'white')
 
         get_word()
         exercise_interval_id = setInterval(get_word, words_delay)
@@ -175,5 +169,4 @@ function start_strup() {
         error_text_node.innerHTML = 'Задайте скорость появления слов от 1 до 10'
         error_text_node.style.setProperty('visibility', 'visible')
     }
-    
 }
